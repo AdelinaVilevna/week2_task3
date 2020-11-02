85430591c065f75bc99a8be194a4a0ed390edc38
AdelinaVilevna <adelina.abdykanyeva@gmail.com>
2020-11-02 23:23:12 +0600

    week2_task3

---
+++ Abdykanyeva_task4.py
@@ -0,0 +1,108 @@
+# Question 1
+
+# lis = ["John", "Jessica", "Jerry", "Tom", "Ben"]
+# for item in lis:
+#     print(item)
+
+
+#Question 2 
+
+# lis = ["Honda", "Kawasaki ", "BMW"]
+# result = "My favorite car is "+ lis[0] + "\n I like " + lis[1] + "motorcycles "
+# print(result)
+
+#Question 3
+# lis = ["str1", "str2"]
+# lis.reverse()
+# print(lis)
+
+# Question 4
+# lis = [1, 2, 3, 4, 5, 6, 7]
+# n = len(lis)// 2
+# z = lis[n:] + lis[:n]
+# print(z)
+
+       #  2 page. Question 1
+# lis = ["John", "Jessica", "Rose"]
+# result = "Hello, " + lis[0] + "!" + " Come for lunch today!"
+# result = "Hello, " + lis[1] + "!" + " Come for lunch today!"
+# result = "Hello, " + lis[2] + "!" + " Come for lunch today!"
+
+# print(result)
+
+
+            #Question 2
+# lis = ["John", "Jessica", "Rose"]
+# result = "Hello, " + lis[0] + "!" + " Come for lunch today!"
+# result = "Hello, " + lis[1] + "!" + " Come for lunch today!"
+# result = "Hello, " + lis[2] + "!" + " Come for lunch today!"
+# print(result)
+# print("Rose can't come today")
+# lis [2] = "Lilly"
+# print(lis)
+# res = "Hello, " + lis[0] + "!" + " Come for lunch today!"
+# res = "Hello, " + lis[1] + "!" + " Come for lunch today!"
+# res = "Hello, " + lis[2] + "!" + " Come for lunch today!"
+# print(res)
+
+#Question 3
+# lis = ["John", "Jessica", "Rose"]
+# lis.insert(0, "Ben")
+# lis.insert(2, "Liz")
+# lis.append("Tomas")
+# print (lis)
+# result = "Hello, " + lis[0] + "!" + " Come for lunch today!"
+# result = "Hello, " + lis[1] + "!" + " Come for lunch today!"
+# result = "Hello, " + lis[2] + "!" + " Come for lunch today!"
+# result = "Hello, " + lis[3] + "!" + " Come for lunch today!"
+# result = "Hello, " + lis[4] + "!" + " Come for lunch today!"
+# result = "Hello, " + lis[5] + "!" + " Come for lunch today!"
+# print(result)
+
+#Question 4
+# lis = ["John", "Jessica", "Rose"]
+# lis.insert(0, "Ben")
+# lis.insert(2, "Liz")
+# lis.append("Tomas")
+# result = "Hello, " + lis[0] + "!" + " Come for lunch today!"
+# result = "Hello, " + lis[1] + "!" + " Come for lunch today!"
+# result = "Hello, " + lis[2] + "!" + " Come for lunch today!"
+# result = "Hello, " + lis[3] + "!" + " Come for lunch today!"
+# result = "Hello, " + lis[4] + "!" + " Come for lunch today!"
+# result = "Hello, " + lis[5] + "!" + " Come for lunch today!"
+# print(result)
+# note_for_quests = "For lunch we can invite only two person"
+# print(note_for_quests)
+# popped_quests = lis.pop()
+# print(lis)
+# message = " We are sorry for inconvenience"
+# print(message)
+# popped_quests = lis.pop()
+# print(lis)
+# message = " We are sorry for inconvenience"
+# print(message)
+# popped_quests = lis.pop()
+# print(lis)
+# message = " We are sorry for inconvenience"
+# print(message)
+# popped_quests = lis.pop()
+# print(lis)
+# message = " We are sorry for inconvenience"
+# result_for_remaning = "The earlier invitation remains valid," + lis[0] + "!"
+# result_for_remaning = "The earlier invitation remains valid," + lis[1] + "!"
+# print(result_for_remaning)
+# del lis [0]
+# del lis [0]
+# print(lis)
+
+
+# Question 5
+# suitcase = []
+# suitcase.append("glasses")
+# suitcase.append("T-shirt")
+# suitcase.append("shirt")
+# suitcase.append("blanket")
+# suitcase.append("shoes")
+# suitcase.pop()
+# suitcase. insert(0, "trousers")
+# print(suitcase)
